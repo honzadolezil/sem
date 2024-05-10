359309a6a82ec242fc76ee1220c168abc6e1d6ba
Jan Dolezil <honza.dolezil@gmail.com>
2024-05-10 17:33:42 +0200

    succesfull refractoring of module

--- module.c
+++ module.c
@@ -24,8 +24,6 @@ void call_termios(int reset);
 #define NUM_CHUNKS 100
 
 typedef struct { // shared date structure;
-    int alarm_period;
-    int alarm_counter;
     bool quit;
     int fd; //forwarding
     int rd;// recieving
@@ -56,54 +54,55 @@ typedef struct { // shared date structure;
 
 
 
-}   data_t;
+} data_t;
+
+// constants definitions
+enum { INPUT, CALCULATION, NUM_THREADS };
+#define CHUNK_SIZE_W 64
+#define CHUNK_SIZE_H 48
 
 void* input_thread(void*);
 void* calculation_thread(void*);
 
+// message comunication
 message *buffer_parse(data_t *data, int message_type);
 bool send_message(data_t *data, message *msg);
+void process_message(data_t* data, uint8_t c);
 
+// julia set computation
 void compute_julia_set(data_t *data);
 
-#define CHUNK_SIZE_W 64
-#define CHUNK_SIZE_H 48
+// thread management functions
+void create_threads(pthread_t threads[], void* (*thr_functions[])(void*), data_t *data, const char *threads_names[]);
+void join_threads(pthread_t threads[], const char *threads_names[]);
+void init_mutex_cond(pthread_mutex_t* mtx, pthread_cond_t* cond, data_t* data);
 
 int main(int argc, char *argv[])
 {
-   data_t data = { .alarm_period = 0, .alarm_counter = 0, .quit = false, .fd = EOF, .is_serial_open = false, .abort = false, .is_cond_signaled = false, .cid = 0, .re = 0, .im = 0, .n_re = 0, .n_im = 0, .is_message_recieved = false, .mtx = NULL, .cond = NULL, .c_re = 0, .c_im = 0, .d_re = 0, .d_im = 0, .n = 0};
+    call_termios(0); // set terminal settings
 
-   enum { INPUT, CALCULATION, NUM_THREADS };
-   const char *threads_names[] = { "Input", "Calculation",};
+    // shared data structure
+    data_t data = {.quit = false, .fd = EOF, .is_serial_open = false, .abort = false, .is_cond_signaled = false, .cid = 0, .re = 0, .im = 0, .n_re = 0, .n_im = 0, .is_message_recieved = false, .mtx = NULL, .cond = NULL, .c_re = 0, .c_im = 0, .d_re = 0, .d_im = 0, .n = 0 };
 
-   void* (*thr_functions[])(void*) = { input_thread, calculation_thread};
+    // thread names and functions
+    const char *threads_names[] = { "Input", "Calculation" };
+    void* (*thr_functions[])(void*) = { input_thread, calculation_thread };
+    pthread_t threads[NUM_THREADS];
 
-   pthread_t threads[NUM_THREADS];
-   pthread_mutex_t mtx;
-   pthread_cond_t cond;
-   pthread_mutex_init(&mtx, NULL); // initialize mutex with default attributes
-   pthread_cond_init(&cond, NULL); // initialize condition variable with default attributes
-   data.mtx = &mtx;                // make the mutex accessible from the shared data structure
-   data.cond = &cond;              // make the cond accessible from the shared data structure
-  
+    // mutex and condition variable
+    pthread_mutex_t mtx;
+    pthread_cond_t cond;
+    init_mutex_cond(&mtx, &cond, &data);   
 
-   call_termios(0);
-   
+    // create threads and wait for their termination
+    create_threads(threads, thr_functions, &data, threads_names);
+    join_threads(threads, threads_names);
 
-   for (int i = 0; i < NUM_THREADS; ++i) { // create threads 
-      int r = pthread_create(&threads[i], NULL, thr_functions[i], &data);
-      printf("\033[1;35mTHREAD\033[0m: Create thread '%s' %s\r\n", threads_names[i], ( r == 0 ? "OK" : "FAIL") );
-   }
-
-   int *ex;
-   for (int i = 0; i < NUM_THREADS; ++i) { // join threads so main doesnt end before threads
-      printf("\033[1;35mTHREAD\033[0m: Call join to the thread %s\r\n", threads_names[i]);
-      int r = pthread_join(threads[i], (void*)&ex);
-      printf("\033[1;35mTHREAD\033[0m: Joining the thread %s has been %s - exit value %i\r\n", threads_names[i], (r == 0 ? "OK" : "FAIL"), *ex);
-   }
-
-   call_termios(1); // restore terminal settings
-   return EXIT_SUCCESS;
+    // clean up
+    pthread_mutex_destroy(&mtx);
+    pthread_cond_destroy(&cond);
+    call_termios(1); // restore terminal settings
+    return EXIT_SUCCESS;
 }
 
 
@@ -118,16 +117,19 @@ void* input_thread(void* d)
         fprintf(stderr, "Error: Unable to open the file %s\r\n", MY_DEVICE_OUT);
         exit(1); // not coding style but whatever
     }
+     
     data->rd= io_open_write(MY_DEVICE_IN);
     if (data->rd == EOF) {
-      fprintf(stderr, "Error: Unable to open the file %s\r\n", MY_DEVICE_IN);
-      exit(1);
+        fprintf(stderr, "Error: Unable to open the file %s\r\n", MY_DEVICE_IN);
+        exit(1);
     }
-
+    
+    printf("INFO: Input thread is running\r\n");
     // wait for recieving startup message
-    while(!data->quit){ 
+
+    while(!data->quit){
         uint8_t c;
-        io_getc_timeout(data->fd, 0,&c); 
+        io_getc_timeout(data->fd, 0,&c);
         if (c == MSG_STARTUP){
             message *msg = buffer_parse(data, MSG_STARTUP);
             printf("INFO: Startup: %s\r\n", msg->data.startup.message);
@@ -140,89 +142,16 @@ void* input_thread(void* d)
             break;
         }
     }
-    printf("INFO: Startup message recieved\r\n");
-    //pthread_mutex_lock(data->mtx);
+    if(!data->quit)
+        printf("INFO: Startup message recieved\r\n");
     while (!data->quit) {
-        
         uint8_t c = '\0';
-        io_getc_timeout(data->fd, 0,&c); 
-        if (c == 'q'){
-            data->quit = true;
-            break;
-        }    
-        else if (c == MSG_GET_VERSION){//sends firmware info
-            printf("INFO: sending version\r\n");
-            //pthread_mutex_unlock(data->mtx);
-            message msg  = {.type = MSG_VERSION, .data.version = {'1','2','2'}};
-            if(!send_message(data,&msg))
-                exit(1);
-            fsync(data->rd);
-           // pthread_mutex_lock(data->mtx);
-        }
-        else if (c == MSG_STARTUP){
-            //pthread_mutex_unlock(data->mtx);
-            message *msg = buffer_parse(data, MSG_STARTUP);
-            printf("INFO: Startup: %s\r\n", msg->data.startup.message);
-            free(msg);
-            c = '\0';
-            //pthread_mutex_lock(data->mtx);
-        }
-        else if (c == MSG_SET_COMPUTE){
-            //pthread_mutex_unlock(data->mtx);
-            printf("INFO: recieved set compute\r\n");
-            message *msg = buffer_parse(data, MSG_SET_COMPUTE);
-            data->c_re = msg->data.set_compute.c_re;
-            data->c_im = msg->data.set_compute.c_im;
-            data->d_re = msg->data.set_compute.d_re;
-            data->d_im = msg->data.set_compute.d_im;
-            data->n = msg->data.set_compute.n;
-
-
-            printf("c_re = %lf, c_im = %lf, d_re = %lf, d_im = %lf, n = %d\r\n", data->c_re, data->c_im, data->d_re, data->d_im, data->n);
-            c = '\0';
-            free(msg);   
-            //pthread_mutex_lock(data->mtx);
-        }
-        else if (c == MSG_COMPUTE){
-            //pthread_mutex_unlock(data->mtx);
-            printf("INFO: recieved compute\r\n");
-            message *msg = buffer_parse(data, MSG_COMPUTE);
-            data->cid = msg->data.compute.cid;
-            data->re = msg->data.compute.re;
-            data->im = msg->data.compute.im;
-            data->n_re = msg->data.compute.n_re;
-            data->n_im = msg->data.compute.n_im;         
-            data->is_cond_signaled = true;
-            data->abort = false;
-            data->is_abort = false;
-            pthread_cond_broadcast(data->cond);
-
-            //pthread_mutex_lock(data->mtx);
-
-            c = '\0';
-            free(msg);
-
-            
-        }
-        else if (c == MSG_ABORT){
-            //printf("recieved end of computation\r\n");
-            data->abort = true;
-            pthread_mutex_unlock(data->mtx);
-            message *msg = buffer_parse(data, MSG_ABORT);
-            free(msg);
-            //pthread_mutex_lock(data->mtx);
-            c = '\0';
-
-        }
-            
+        io_getc_timeout(data->fd, 0, &c);
+        process_message(data, c);
     }
-      
-    //pthread_mutex_unlock(data->mtx);
     data->quit = true;
     r = 1;
     pthread_cond_broadcast(data->cond);
-    
-    //pthread_mutex_unlock(data->mtx);
     fprintf(stderr, "\033[1;35mTHREAD\033[0m: Exit input thread %lu\r\n", (unsigned long)pthread_self());
     return &r;
 }
@@ -251,7 +180,7 @@ void* calculation_thread(void*d){
             double start_re = data->re;
             double start_im = data->im;
             while(!q){
-                
+
                 if(data->cid == 100){
                     printf("INFO: Calculation thread is done\r\n");
                     pthread_mutex_unlock(data->mtx);
@@ -272,7 +201,7 @@ void* calculation_thread(void*d){
                 //printf("INFO: Chunk %d: x_im = %d, y_im = %d\r\n", data->cid, x_im, y_im);
                 data->re =start_re + x_im * data->d_re;
                 data->im = start_im + y_im * data->d_im;
-                //printf("INFO: Chunk %d: re = %lf, im = %lf\r\n", data->cid, data->re, data->im);                
+                //printf("INFO: Chunk %d: re = %lf, im = %lf\r\n", data->cid, data->re, data->im);
 
                 compute_julia_set(data);
                 if(data->is_abort){
@@ -293,16 +222,16 @@ void* calculation_thread(void*d){
 }
 
 bool send_message(data_t *data, message *msg){
-   uint8_t msg_buf[sizeof(message)];
-   int size;
-   fill_message_buf(msg, msg_buf,sizeof(message), &size);
-   pthread_mutex_lock(data->mtx);
-   int ret = write(data->rd, msg_buf, size);
-   pthread_mutex_unlock(data->mtx);
-   if(size != ret){
+    uint8_t msg_buf[sizeof(message)];
+    int size;
+    fill_message_buf(msg, msg_buf,sizeof(message), &size);
+    pthread_mutex_lock(data->mtx);
+    int ret = write(data->rd, msg_buf, size);
+    pthread_mutex_unlock(data->mtx);
+    if(size != ret){
         exit(1);
-   }
-   return size == ret;
+    }
+    return size == ret;
 }
 
 message *buffer_parse(data_t *data, int message_type){
@@ -312,7 +241,7 @@ message *buffer_parse(data_t *data, int message_type){
     int i = 0;
 
     get_message_size(message_type, &len);
-    msg_buf[i++] = message_type; // add the first byte 
+    msg_buf[i++] = message_type; // add the first byte
     while((i < len)){
         io_getc_timeout(data->fd, 0, &c);
         msg_buf[i++] = c;
@@ -329,35 +258,35 @@ message *buffer_parse(data_t *data, int message_type){
         message msg2  = {.type = MSG_ERROR};
         send_message(data,&msg2);
         fsync(data->rd);
-        free(msg);  
+        free(msg);
         exit(1);
-    } 
+    }
     return msg;
 }
 
 void call_termios(int reset)
 {
-   static struct termios tio, tioOld;
-   tcgetattr(STDIN_FILENO, &tio);
-   if (reset) {
-      tcsetattr(STDIN_FILENO, TCSANOW, &tioOld);
-   } else {
-      tioOld = tio; //backup 
-      cfmakeraw(&tio);
-      tcsetattr(STDIN_FILENO, TCSANOW, &tio);
-   }
+    static struct termios tio, tioOld;
+    tcgetattr(STDIN_FILENO, &tio);
+    if (reset) {
+        tcsetattr(STDIN_FILENO, TCSANOW, &tioOld);
+    } else {
+        tioOld = tio; //backup
+        cfmakeraw(&tio);
+        tcsetattr(STDIN_FILENO, TCSANOW, &tio);
+    }
 }
 
 
 
 void compute_julia_set(data_t *data) {
-    
+
     uint8_t iter;
     double complex Z;
     double complex C = data->c_re + data->c_im * I;
-    for (uint8_t x = 0; x <= CHUNK_SIZE_W; x++) { // for size of chunk 
+    for (uint8_t x = 0; x <= CHUNK_SIZE_W; x++) { // for size of chunk
         for (uint8_t y = 0; y <= CHUNK_SIZE_H; y++) { // for size of chunk
-            Z = (data->re + x * data->d_re) + (data->im + y * data->d_im) * I; 
+            Z = (data->re + x * data->d_re) + (data->im + y * data->d_im) * I;
             iter = 0;
             while (cabs(Z) < 2 && iter < data->n) {
                 Z = Z * Z + C;
@@ -384,10 +313,80 @@ void compute_julia_set(data_t *data) {
 
 
         }
-        
+
     }
-    printf("INFO: Chunk %d is done\r\n", data->cid);
+    printf("INFO: Chunk %d is done\n\r", data->cid);
+}
+
+void create_threads(pthread_t threads[], void* (*thr_functions[])(void*), data_t *data, const char *threads_names[]) {
+    for (int i = 0; i < NUM_THREADS; ++i) { // create threads
+        int r = pthread_create(&threads[i], NULL, thr_functions[i], data);
+        printf("\033[1;35mTHREAD\033[0m: Create thread '%s' %s\r\n", threads_names[i], ( r == 0 ? "OK" : "FAIL") );
+    }
+}
+
+void join_threads(pthread_t threads[], const char *threads_names[]) {
+    int *ex;
+    for (int i = 0; i < NUM_THREADS; ++i) { // join threads so main doesnt end before threads
+        printf("\033[1;35mTHREAD\033[0m: Call join to the thread %s\r\n", threads_names[i]);
+        int r = pthread_join(threads[i], (void*)&ex);
+        printf("\033[1;35mTHREAD\033[0m: Joining the thread %s has been %s - exit value %i\r\n", threads_names[i], (r == 0 ? "OK" : "FAIL"), *ex);
+    }
+}
+
+void init_mutex_cond(pthread_mutex_t* mtx, pthread_cond_t* cond, data_t* data) {
+    pthread_mutex_init(mtx, NULL); // initialize mutex with default attributes
+    pthread_cond_init(cond, NULL); // initialize condition variable with default attributes
+    data->mtx = mtx;               // make the mutex accessible from the shared data structure
+    data->cond = cond;
+}
 
 
-    
+void process_message(data_t* data, uint8_t c) {
+    if (c == 'q'){
+        data->quit = true;
+    }
+    else if (c == MSG_GET_VERSION){
+        printf("INFO: sending version\r\n");
+        message msg  = {.type = MSG_VERSION, .data.version = {'1','2','2'}};
+        if(!send_message(data,&msg))
+            exit(1);
+        fsync(data->rd);
+    }
+    else if (c == MSG_STARTUP){
+        message *msg = buffer_parse(data, MSG_STARTUP);
+        printf("INFO: Startup: %s\r\n", msg->data.startup.message);
+        free(msg);
+    }
+    else if (c == MSG_SET_COMPUTE){
+        printf("INFO: received set compute\r\n");
+        message *msg = buffer_parse(data, MSG_SET_COMPUTE);
+        data->c_re = msg->data.set_compute.c_re;
+        data->c_im = msg->data.set_compute.c_im;
+        data->d_re = msg->data.set_compute.d_re;
+        data->d_im = msg->data.set_compute.d_im;
+        data->n = msg->data.set_compute.n;
+        printf("c_re = %lf, c_im = %lf, d_re = %lf, d_im = %lf, n = %d\r\n", data->c_re, data->c_im, data->d_re, data->d_im, data->n);
+        free(msg);
+    }
+    else if (c == MSG_COMPUTE){
+        printf("INFO: received compute\r\n");
+        message *msg = buffer_parse(data, MSG_COMPUTE);
+        data->cid = msg->data.compute.cid;
+        data->re = msg->data.compute.re;
+        data->im = msg->data.compute.im;
+        data->n_re = msg->data.compute.n_re;
+        data->n_im = msg->data.compute.n_im;
+        data->is_cond_signaled = true;
+        data->abort = false;
+        data->is_abort = false;
+        pthread_cond_broadcast(data->cond);
+        free(msg);
+    }
+    else if (c == MSG_ABORT){
+        data->abort = true;
+        pthread_mutex_unlock(data->mtx);
+        message *msg = buffer_parse(data, MSG_ABORT);
+        free(msg);
+    }
 }
